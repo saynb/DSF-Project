d8fb498ca6445e702cbd4f08ce419aa04138c094
sabandyopadh <sabandyopadh@cs.stonybrook.edu>
2016-11-14 00:23:35 -0500

    Description: Adding initial files -> 1. 2 txt files which contain URL list of goodread books to scrape 2. Code to crawl the links to get URLs of all the books. 3. Code to get all the initial data from one URL into a pd DataFrame.

---
+++ crawl.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+book_better = open('book_was_better.txt','wb')
+movie_better = open('movie_was_better.txt','wb')
+
+def clean_text(text):
+    return ' '.join(text.split())
+
+def get_content(link, f):
+    movie_page = requests.get(link)
+    soup = BeautifulSoup(movie_page.content, "lxml")
+
+    for row in soup.find_all('a',class_='bookTitle'):
+        f.write('www.goodreads.com'+row.get('href')+'\n')
+
+link = "https://www.goodreads.com/list/show/429.The_BOOK_was_BETTER_than_the_MOVIE?page="
+
+for i in xrange(1,15):
+    link1 = link+str(i)
+    get_content(link1,book_better)
+
+link = "https://www.goodreads.com/list/show/104.The_MOVIE_was_BETTER_than_the_BOOK?page="
+
+for i in xrange(1,10):
+    link1 = link+str(i)
+    get_content(link1,movie_better)
+
+book_better.close()
+movie_better.close()
